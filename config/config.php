e108c3951a174382317eec331f0315cefb4a194d
manuelm1209 <55123690+manuelm1209@users.noreply.github.com>
2021-05-17 16:48:13 -0500

    Delete config.php

--- config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('URL', 'http://18.208.112.131:80/');
-define('URL_S3', 'https://mentors-day-assets.s3-us-west-2.amazonaws.com/assets/');
-define('HOST', 'localhost');
-define('DB', 'endeavor_mentors_live');
-define('USER', 'root');
-define('PASSWORD', 'IvU7qGa9ZUah');
